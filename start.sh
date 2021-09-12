7242a628f5bd6714f785906197350a8169a0834a
Thor-Ragnarokk <88768778+Thor-Ragnarokk@users.noreply.github.com>
2021-09-12 20:01:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
-cd /RadioPlayerV3
+git clone https://github.com/Thor-Ragnarokk/Dps_MUSICA_WORLDV2.git /Dps_MUSICA_WORLDV2
+cd /Dps_MUSICA_WORLDV2
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
